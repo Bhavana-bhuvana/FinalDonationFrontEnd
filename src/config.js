b44b7d38fc6550f90846deef8c305aace7752b27
Bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-11-15 15:55:57 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-  API_URL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
+  API_URL: import.meta.env.VITE_API_URL,
   Razor_Pay:import.meta.env.VITE_RAZORPAY_KEY_ID,
 };
 
