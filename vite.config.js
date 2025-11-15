a3c5bff092e59dd33237db69ec83cbe7903d081f
Bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-11-15 13:36:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // change 'dist' to 'build' if you prefer
+    outDir: 'dist', // change 'dist' to 'build' if you prefer
   },
   base: '/',
 })
